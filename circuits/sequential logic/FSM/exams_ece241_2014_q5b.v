1f308785e2ea5140363560ed8edfb3128f94881d
BARTNeko <github@ling.moe>
2024-08-02 00:08:13 +0800

    upload solution for Q5b: Serial two's complementer (Mealy FSM)

---
+++ circuits/sequential logic/FSM/exams_ece241_2014_q5b.v
@@ -0,0 +1,23 @@
+module top_module (
+    input clk,
+    input areset,
+    input x,
+    output z
+); 
+    parameter A = 0, B = 1;
+    reg state, next_state;
+
+    always @(*) begin
+        case(state)
+            A: next_state = x ? B : A;
+            B: next_state = B;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        if(areset) state <= A;
+        else state <= next_state;
+    end
+
+    assign z = (state == A) ? x : ~x;
+endmodule
