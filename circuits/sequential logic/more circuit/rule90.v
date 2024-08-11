5cd5f5b02218f51f83d2efb767367126da803de8
Ling <github@ling.moe>
2024-08-11 17:00:41 +0800

    fix formatting

--- circuits/sequential logic/more circuit/rule90.v
+++ circuits/sequential logic/more circuit/rule90.v
@@ -4,4 +4,16 @@ module top_module(
     input [511:0] data,
     output [511:0] q ); 
 
+    int i;
+    
+    always @(posedge clk) begin
+        if (load) q <= data;
+        else begin
+            for (i = 1; i < 511; i++)
+                q[i] <= q[i-1] ^ q[i+1];
+            
+            q[0] <= q[1] ^ 1'b0;
+            q[511] <= q[510] ^ 1'b0;
+        end
+    end
 endmodule
