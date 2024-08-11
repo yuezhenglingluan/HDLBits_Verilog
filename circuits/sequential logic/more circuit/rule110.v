02b93d46b368cc1f7133ba41a635e82486812083
Ling <github@ling.moe>
2024-08-11 17:03:12 +0800

    upload solution for Rule 110

--- circuits/sequential logic/more circuit/rule110.v
+++ circuits/sequential logic/more circuit/rule110.v
@@ -5,4 +5,17 @@ module top_module(
     output [511:0] q
 ); 
 
+    int i;
+    
+    always @(posedge clk) begin
+        if (load) q <= data;
+        else begin
+            for (i = 1; i < 511; i++)
+                q[i] <= (q[i-1] | q[i]) & (~q[i-1] | ~q[i] | ~q[i+1]);
+            
+            q[0] <= q[0];
+            q[511] <= q[510] | q[511];
+        end
+    end
+
 endmodule
