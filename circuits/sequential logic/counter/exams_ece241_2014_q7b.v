98ce56f83b69cbca6e6f72643264e881196c31b8
Ling <github@ling.moe>
2024-08-11 16:19:59 +0800

    upload solution for Counter 1000

--- circuits/sequential logic/counter/exams_ece241_2014_q7b.v
+++ circuits/sequential logic/counter/exams_ece241_2014_q7b.v
@@ -5,7 +5,15 @@ module top_module (
     output [2:0] c_enable
 ); //
 
-    bcdcount counter0 (clk, reset, c_enable[0]/*, ... */);
-    bcdcount counter1 (clk, reset, c_enable[1]/*, ... */);
+    // bcdcount counter0 (clk, reset, c_enable[0]/*, ... */);
+    // bcdcount counter1 (clk, reset, c_enable[1]/*, ... */);
 
+    wire [3:0] out0, out1, out2;
+
+    assign c_enable = {((out1 == 4'h9) && (out0 == 4'h9)), (out0 == 4'h9),1'b1};
+    assign OneHertz = ((out0 == 4'h9) && (out1 == 4'h9) && (out2 == 4'h9));
+
+    bcdcount counter0 (clk, reset, c_enable[0], out0);
+    bcdcount counter1 (clk, reset, c_enable[1], out1);
+    bcdcount counter2 (clk, reset, c_enable[2], out2);
 endmodule
